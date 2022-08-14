a647e424cfcadf85afc65bda621ff3dcc053defa
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-14 23:21:29 +0800

    work on date

--- src/screens/App/Goals/index.js
+++ src/screens/App/Goals/index.js
@@ -10,25 +10,25 @@ import { Picker } from "@react-native-picker/picker";
 
 const GoalScreen = ({ navigation }) => {
   const { theme } = useTheme();
-  const [categories, setCategories] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [error, setError] = useState();
   const [categoryName, setCategoryName] = useState("");
   const [visible, setVisible] = useState(false);
-  const [country, setCountry] = useState("Unknown");
+  const [country, setCountry] = useState("unknown");
 
   const [value, setValue] = useState("");
 
   useEffect(() => {
-    fetchCategories();
+    fetchTasks();
   }, []);
 
   const toggleDialog = () => {
     setVisible(!visible);
   };
 
-  const fetchCategories = async () => {
+  const fetchTasks = async () => {
     const response = await fetch(
-      `${Constants.manifest.extra.baseUrl}/api/v1/categories`,
+      `${Constants.manifest.extra.baseUrl}/api/v1/task`,
       {
         method: "GET",
         headers: {
@@ -37,41 +37,43 @@ const GoalScreen = ({ navigation }) => {
       }
     );
     const json = await response.json();
-    // console.log("CATEOGIRES: ", json.data)
-    setCategories(json.data);
+    console.log("REPSONE: ", json);
+    setTasks(json);
   };
 
-  const toggleCategory = async (category) => {
-    try {
-      await axios.post(
-        `${Constants.manifest.extra.baseUrl}/api/v1/categories/update`,
-        {
-          active: !category.active,
-          categoryId: category.id,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${JwtService.accessToken}`,
-          },
-        }
-      );
-      ToastAndroid.show("Updated!", ToastAndroid.SHORT);
-    } catch (err) {
-      console.log("X: ", err);
-    }
-  };
+  // const toggleCategory = async (category) => {
+  //   try {
+  //     await axios.post(
+  //       `${Constants.manifest.extra.baseUrl}/api/v1/tasks/update`,
+  //       {
+  //         active: !category.active,
+  //         categoryId: category.id,
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${JwtService.accessToken}`,
+  //         },
+  //       }
+  //     );
+  //     ToastAndroid.show("Updated!", ToastAndroid.SHORT);
+  //   } catch (err) {
+  //     console.log("X: ", err);
+  //   }
+  // };
 
-  const handleToggleSwitch = (category) => async () => {
-    await toggleCategory(category);
-    await fetchCategories();
-  };
+  // const handleToggleSwitch = (category) => async () => {
+  //   await toggleCategory(category);
+  //   await fetchTasks();
+  // };
 
   const onSubmitCategory = async () => {
     try {
       await axios.post(
-        `${Constants.manifest.extra.baseUrl}/api/v1/categories`,
+        `${Constants.manifest.extra.baseUrl}/api/v1/task`,
         {
-          name: categoryName,
+          title: categoryName,
+          categoryId: 1,
+          description: "",
         },
         {
           headers: {
@@ -82,9 +84,17 @@ const GoalScreen = ({ navigation }) => {
     } catch (err) {
       console.log("X: ", err);
     }
-    await fetchCategories();
+    setCountry("unknown");
+    await fetchTasks();
     setCategoryName("");
     setVisible(false);
+  };
+
+  const onActionSelect = (value, index) => {
+    if (value === "add") {
+      setVisible(true);
+    }
+    setCountry(value);
   };
   return (
     <View style={styles.container}>
@@ -99,35 +109,41 @@ const GoalScreen = ({ navigation }) => {
           flexDirection: "row",
         }}
       >
-        <Text style={{ fontWeight: "700", fontSize: 30 }}>Goals</Text>
-        {/* <Picker
-        selectedValue={country}
-        onValueChange={(value, index) => setCountry(value)}
-        mode="dropdown" // Android only
-        style={styles.picker}
-      >
-        <Picker.Item label="Please select your country" value="Unknown" />
-        <Picker.Item label="Australia" value="Australia" />
-        <Picker.Item label="Belgium" value="Belgium" />
-        <Picker.Item label="Canada" value="Canada" />
-        <Picker.Item label="India" value="India" />
-        <Picker.Item label="Japan" value="Japan" />
-      </Picker> */}
+        <Text h4>All Tasks</Text>
+        <View
+          style={{
+            backgroundColor: theme.colors.primary,
+            borderRadius: 12,
+            height: 40,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Picker
+            selectedValue={country}
+            onValueChange={onActionSelect}
+            mode="dropdown" // Android only
+            style={styles.picker}
+          >
+            <Picker.Item label="Actions" value="unknown" />
+            <Picker.Item label="Add Task" value="add" />
+          </Picker>
+        </View>
       </View>
 
-      <View style={{ paddingLeft: 30, paddingRight: 30, marginTop: 10 }}>
-        {categories.length > 0 ? (
-          categories.map((category, i) => (
-            <ListItem key={i}>
+      <View style={{ paddingLeft: 10, paddingRight: 30, marginTop: 10 }}>
+        {tasks.length > 0 ? (
+          tasks.map((task, i) => (
+            <ListItem key={i} bottomDivider>
               <ListItem.Content>
-                <ListItem.Title style={{ fontSize: 20, fontWeight: "600" }}>
-                  {category.name}ss
+                <ListItem.Title style={{ fontSize: 16, fontWeight: "600" }}>
+                  {task.title}
                 </ListItem.Title>
               </ListItem.Content>
-              <Switch
+              {/* <Switch
                 onValueChange={handleToggleSwitch(category)}
                 value={category.active}
-              />
+              /> */}
             </ListItem>
           ))
         ) : (
@@ -136,15 +152,12 @@ const GoalScreen = ({ navigation }) => {
       </View>
       <StatusBar style="auto" />
       <Dialog isVisible={visible} onBackdropPress={toggleDialog}>
-        <Dialog.Title title="Create a new Goal" />
-        <Text>
-          These can be toggled to change visibility, depending on your focus
-        </Text>
+        <Dialog.Title title="Add a new task" />
         <Input
           onChangeText={(value) => setCategoryName(value)}
           value={categoryName}
           nativeID="categoryName"
-          placeholder="Enter Cateogry Name..."
+          placeholder="Task name..."
           style={{ marginTop: 15 }}
         />
         <Button
@@ -166,10 +179,10 @@ const styles = StyleSheet.create({
     paddingTop: 15,
   },
   picker: {
-    width: 200,
-    padding: 10,
+    width: 150,
+    color: "white",
+    borderRadius: 8,
     borderWidth: 1,
-    borderColor: "#666",
   },
 });
 
