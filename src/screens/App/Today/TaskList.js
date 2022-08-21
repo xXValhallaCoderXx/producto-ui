048b90f262398d2c7654c0a2c7a9f1af946f62f4
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-21 19:39:43 +0800

    Fix toggle complete

--- src/screens/App/Today/TaskList.js
+++ src/screens/App/Today/TaskList.js
@@ -1,9 +1,14 @@
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import IoniIcons from "react-native-vector-icons/Ionicons";
 
 import { ListItem, Text, useTheme, CheckBox } from "@rneui/themed";
 
-const TaskList = ({ editMode, tasks, handleToggleSwitch, handleToggleFocus }) => {
+const TaskList = ({
+  editMode,
+  tasks,
+  handleToggleSwitch,
+  handleToggleFocus,
+}) => {
   const { theme } = useTheme();
   return (
     <View>
@@ -15,51 +20,53 @@ const TaskList = ({ editMode, tasks, handleToggleSwitch, handleToggleFocus }) =>
           </Text>
         </Text>
       ) : (
-        tasks
-          .filter((task) => {
-            if (!editMode && !task.focus && !task.completed) {
-              return false;
-            }
-            return task;
-          })
-          .map((task, index) => {
-            return (
-              <ListItem key={index} bottomDivider>
-                <ListItem.Content
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <ListItem.Title>{task.title}</ListItem.Title>
-                  <View
+        <ScrollView style={{ height: 400 }}>
+          {tasks
+            .filter((task) => {
+              if (!editMode && !task.focus && !task.completed) {
+                return false;
+              }
+              return task;
+            })
+            .map((task, index) => {
+              return (
+                <ListItem key={index} bottomDivider>
+                  <ListItem.Content
                     style={{
                       display: "flex",
-                      alignItems: "center",
                       flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
                     }}
                   >
-                    {editMode && (
-                      <IoniIcons
-                        style={{ fontSize: 20, marginRight: 10 }}
-                        color={task.focus ? theme.colors.primary : "black"}
-                        name={"key"}
-                        onPress={handleToggleFocus(task)}
+                    <ListItem.Title>{task.title}</ListItem.Title>
+                    <View
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        flexDirection: "row",
+                      }}
+                    >
+                      {editMode && (
+                        <IoniIcons
+                          style={{ fontSize: 20, marginRight: 10 }}
+                          color={task.focus ? theme.colors.primary : "black"}
+                          name={"key"}
+                          onPress={handleToggleFocus(task)}
+                        />
+                      )}
+                      <CheckBox
+                        checked={task.completed}
+                        containerStyle={{ padding: 0 }}
+                        onPress={handleToggleSwitch(task)}
                       />
-                    )}
-                    <CheckBox
-                      checked={task.completed}
-                      containerStyle={{ padding: 0 }}
-                      onPress={handleToggleSwitch(task)}
-                    />
-                  </View>
-                  {/* <ListItem.Subtitle>what</ListItem.Subtitle> */}
-                </ListItem.Content>
-              </ListItem>
-            );
-          })
+                    </View>
+                    {/* <ListItem.Subtitle>what</ListItem.Subtitle> */}
+                  </ListItem.Content>
+                </ListItem>
+              );
+            })}
+        </ScrollView>
       )}
     </View>
   );
