33d1b6fd2967ad230fc5defa399f20f69d6cd4d1
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-21 17:27:30 +0800

    Comment out rtk query

--- src/config/store.js
+++ src/config/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todaySlice from "../screens/App/Today/today-slice";
-import { api } from "../api";
+// import { api } from "../api";
 export default configureStore({
   reducer: {
     today: todaySlice,
-    [api.reducerPath]: api.reducer,
+    // [api.reducerPath]: api.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(api.middleware),
 });
