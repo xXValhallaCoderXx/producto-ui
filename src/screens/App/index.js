e3f0f737253d6bd42a201f4e29926b28b3a71257
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-31 03:10:15 +0800

    Work on calendar

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -3,12 +3,12 @@ import DashboardScreen from "./Today";
 import ProfileScreen from "./Profile";
 import { Ionicons, Feather } from "@expo/vector-icons";
 import { useTheme } from "@rneui/themed";
+import * as Haptics from "expo-haptics";
 
 const Tab = createBottomTabNavigator();
 
-export default function App({navigation}) {
+export default function App({ navigation }) {
   const { theme } = useTheme();
-
 
   return (
     <Tab.Navigator
@@ -16,14 +16,33 @@ export default function App({navigation}) {
         headerShown: false,
         tabBarActiveTintColor: theme.colors.primary,
         tabBarInactiveTintColor: "gray",
+
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
           if (route.name === "Today") {
             iconName = "list";
           } else if (route.name === "Account") {
-            return <Ionicons name="person" size={size} color={color} />;
+            return (
+              <Ionicons
+                // onPress={() =>
+                //   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+                // }
+                name="person"
+                size={size}
+                color={color}
+              />
+            );
           }
-          return <Feather name={iconName} size={size} color={color} />;
+          return (
+            <Feather
+              // onPress={() =>
+              //   Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
+              // }
+              name={iconName}
+              size={size}
+              color={color}
+            />
+          );
         },
       })}
     >
