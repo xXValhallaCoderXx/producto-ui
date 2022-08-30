2aff1e66856a74540a4e33a4b397273591aa69a9
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-08-31 03:50:56 +0800

    Clean up component

--- src/screens/App/Today/Calendar.js
+++ src/screens/App/Today/Calendar.js
@@ -43,48 +43,16 @@ const CalendarWidget = ({ calendarOpen, toggleCalendar }) => {
       <TouchableOpacity style={styles.modalContainer} onPress={toggleCalendar}>
         <TouchableOpacity
           style={styles.modal}
-      
           activeOpacity={1}
         >
           <Calendar
-            // Initially visible month. Default = now
             initialDate={selected}
-            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            // minDate={"2012-05-10"}
-            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            // maxDate={"2012-05-30"}
-            // Handler which gets executed on day press. Default = undefined
             onDayPress={onDayPress}
-            // Handler which gets executed on day long press. Default = undefined
-
             // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
             monthFormat={"yyyy MM"}
-            // Handler which gets executed when visible month changes in calendar. Default = undefined
-            // onMonthChange={(month) => {
-            //   console.log("month changed", month);
-            // }}
-            // // Hide month navigation arrows. Default = false
-
-            // Replace default arrows with custom ones (direction can be 'left' or 'right')
-            // renderArrow={(direction) => <Arrow />}
-            // Do not show days of other months in month page. Default = false
             hideExtraDays={true}
-            // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-            // day from another month that is visible in calendar page. Default = false
-            // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
             firstDay={1}
-            // Hide day names. Default = false
-
-            // Show week numbers to the left. Default = false
-
-            // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-            // onPressArrowLeft={(subtractMonth) => subtractMonth()}
-            // // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-            // onPressArrowRight={(addMonth) => addMonth()}
-
-            // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
             disableAllTouchEventsForDisabledDays={true}
-            // Enable the option to swipe between months. Default = false
             enableSwipeMonths={true}
           />
         </TouchableOpacity>
