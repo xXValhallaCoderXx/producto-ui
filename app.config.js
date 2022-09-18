b8b79fd941a5a1dba8a1c65848cddda14dd029e9
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-09-19 03:37:35 +0800

    Test splash screen stuff

--- app.config.js
+++ app.config.js
@@ -1,13 +1,19 @@
-export default {
-  name: "Producto",
-  version: "1.0.0",
-  "android": {
-    "package": "com.xxvalhallacoderxx.producto"
-  },
-  extra: {
-    // environment: "production",
-    environment: "development",
-    baseUrl: "http://10.0.2.2:3000",
-    // baseUrl: "https://producto-dev.herokuapp.com"
-  },
+// export default {
+
+// };
+
+module.exports = ({ config }) => {
+  console.log(config); // prints 'My App'
+  return {
+    ...config,
+    android: {
+      package: "com.xxvalhallacoderxx.producto",
+    },
+    extra: {
+      // environment: "production",
+      environment: "development",
+      baseUrl: "http://10.0.2.2:3000",
+      // baseUrl: "https://producto-dev.herokuapp.com",
+    },
+  };
 };
