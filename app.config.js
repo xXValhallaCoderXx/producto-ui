0018abaaf2e7e705ed443e8e1cc2c3807c55d922
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-10-02 13:28:22 +0800

    Work on app start

--- app.config.js
+++ app.config.js
@@ -1,14 +1,23 @@
 module.exports = ({ config }) => {
   return {
     ...config,
-    android: {
-      package: "com.xxvalhallacoderxx.producto",
+    runtimeVersion: {
+      policy: "sdkVersion",
     },
+    // android: {
+    //   package: "com.xxvalhallacoderxx.producto",
+    //   softwareKeyboardLayoutMode: "pan",
+    // },
+    // ios: {
+    //   supportsTablet: true,
+    //   bundleIdentifier:"com.xxvalhallacoderxx.producto",
+    //   buildNumber: "1.0.0",
+    // },
     extra: {
-      // environment: "development",
-      // baseUrl: "http://10.0.2.2:3000",
-      environment: "production",
-      baseUrl: "https://producto-dev.herokuapp.com",
+      environment: "development",
+      baseUrl: "http://10.0.2.2:3000",
+      // environment: "production",
+      // baseUrl: "https://producto-dev.herokuapp.com",
     },
   };
 };
