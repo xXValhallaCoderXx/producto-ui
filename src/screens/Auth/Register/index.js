b1a03d10d0e1dfa70b0e1e38ed7fa39eb1efd891
xXValhallaCoderXx <renate.gouveia@gmail.com>
2022-10-09 01:03:54 +0800

    Fix registration

--- src/screens/Auth/Register/index.js
+++ src/screens/Auth/Register/index.js
@@ -1,159 +1,233 @@
 import { useState, useEffect } from "react";
-import { useTheme } from "@rneui/themed";
 import { useDispatch } from "react-redux";
-import { TextInput } from "react-native";
+import * as Yup from "yup";
 import * as SecureStore from "expo-secure-store";
 import { useRegisterMutation } from "../../../api/auth-api";
-import { Text, Button } from "@rneui/themed";
+import ProductoButton from "../../../components/Button";
+import { useFormik } from "formik";
+import { TextInput, Text, useTheme } from "react-native-paper";
+import LayoutView from "../../../components/LayoutView";
 import { toggleIsAuthenticated } from "../../../shared/slice/global-slice";
 import { StyleSheet, Platform, View, ToastAndroid, Image } from "react-native";
 import {
   JWT_KEY_STORE,
   REFRESH_JWT_KEY_STORE,
 } from "../../../shared/constants";
+import { useToast } from "react-native-toast-notifications";
 
 const RegisterScreen = ({ navigation }) => {
-  const { theme } = useTheme();
+  const theme = useTheme();
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const toast = useToast();
   const [registerApi, registerApiResult] = useRegisterMutation();
+  const [secretMap, setSecretMap] = useState({
+    password: true,
+    confirmPassword: true,
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().required("Email field is required").email(),
+      password: Yup.string()
+        .min(6, "Please enter a minimum of 6 characters")
+        .max(50, "Password exceeded 50 characters")
+        .required("Password field is required"),
+      confirmPassword: Yup.string()
+        .min(6, "Please enter a minimum of 6 characters")
+        .max(50, "Password exceeded 50 characters")
+        .required("Confirm Password field is required"),
+    }),
+
+    onSubmit: async ({ email, password }) => {
+      const result = await registerApi({ email, password });
+      if (result?.data?.data) {
+        const { accessToken, refreshToken } = result.data.data;
+        await SecureStore.setItemAsync(JWT_KEY_STORE, accessToken);
+        await SecureStore.setItemAsync(REFRESH_JWT_KEY_STORE, refreshToken);
+        dispatch(toggleIsAuthenticated(true));
+      }
+    },
+  });
+
+  const handlePassToggle = (key) => () => {
+    setSecretMap({
+      ...secretMap,
+      [key]: secretMap[key] ? false : true,
+    });
+  };
 
   useEffect(() => {
     if (registerApiResult.isError) {
-      ToastAndroid.show("Error registering user", ToastAndroid.SHORT);
+      toast.show("Email succesffully updated", {
+        type: "success",
+        duration: 2500,
+        offset: 30,
+        animationType: "zoom-in",
+        placement: "bottom",
+        title: "Error Registering User!",
+        description: "Please try something else",
+      });
     }
   }, [registerApiResult.isError]);
 
   useEffect(() => {
     if (registerApiResult.isSuccess) {
-      ToastAndroid.show("Registration success", ToastAndroid.SHORT);
-      storeCredentials();
+      toast.show("Email succesffully updated", {
+        type: "success",
+        duration: 2500,
+        offset: 30,
+        animationType: "zoom-in",
+        placement: "bottom",
+        title: "Registration Success!",
+        description: "Welcome to Productö",
+      });
     }
   }, [registerApiResult.isSuccess]);
 
-  const storeCredentials = async () => {
-    const { accessToken, refreshToken } = registerApiResult.data.data;
-    await SecureStore.setItemAsync(JWT_KEY_STORE, accessToken);
-    await SecureStore.setItemAsync(REFRESH_JWT_KEY_STORE, refreshToken);
-    dispatch(toggleIsAuthenticated(true));
-  };
-
-  const handleOnSubmit = async () => {
-    setError("");
-
-    if (email && password) {
-      await registerApi({ email, password });
-    }
-  };
-
-  useEffect(() => {
-    if (password !== confirmPassword) {
-      setError("Passwords do not match!");
-    } else {
-      setError("");
-    }
-  }, [confirmPassword, password]);
-
+  console.log("FORMIK: ", formik);
   return (
-    <View
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      <Image
-        style={{ height: 40, width: 220, marginBottom: 40 }}
-        source={require("../../../assets/images/title-dark.png")}
-      />
-
-      <TextInput
-        style={styles.input}
-        onChangeText={(value) => {
-          setError("");
-          setEmail(value);
-        }}
-        value={email}
-        nativeID="email"
-        placeholder="Email"
-      />
-      <TextInput
-        style={{ ...styles.input, marginTop: 20, marginBottom: 20 }}
-        onChangeText={(value) => {
-          setError("");
-          setPassword(value);
-        }}
-        value={password}
-        nativeID="password"
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(value) => setConfirmPassword(value)}
-        value={confirmPassword}
-        nativeID="confirmPassword"
-        placeholder="Confirm Password"
-        secureTextEntry={true}
-      />
-      <View style={{ height: 20, marginTop: 10, marginBottom: 10 }}>
-        {(error && (
-          <Text
-            style={{
-              color: "#D14343",
-              textAlign: "center",
-              fontWeight: "700",
-            }}
-          >
-            {error}
-          </Text>
-        )) ||
-          null}
-      </View>
-      {/* 
-       Object.entries(errors)
-      {(error && (
-        <Text
-          style={{
-            color: "#f43a3a",
-            textAlign: "center",
-            fontWeight: "700",
-            marginTop: 5,
-            marginBottom: 10,
-          }}
-        >
-          {error}
-        </Text>
-      )) ||
-        null} */}
-      <Button
-        loading={registerApiResult.isLoading}
-        buttonStyle={{
-          borderRadius: 8,
-          padding: 10,
-          minWidth: 200,
-        }}
-        // disabled={!email || !password || (!confirmPassword && !error)}
-        title="Register"
-        onPress={handleOnSubmit}
-        color="primary"
-      />
-
-      <Text style={{ color: theme.colors.primary, marginTop: 20 }} h5>
-        Already have an account?
-      </Text>
-      <Text
-        h6
-        onPress={() => navigation.navigate("Login")}
+    <LayoutView>
+      <View
         style={{
-          color: "black",
-          marginTop: 5,
-          fontWeight: "700",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 50,
         }}
       >
-        Log-in Here
-      </Text>
-    </View>
+        <Image
+          style={{ height: 40, width: 220, marginBottom: 40 }}
+          source={require("../../../assets/images/title-dark.png")}
+        />
+      </View>
+
+      <View style={{ alignItems: "center" }}>
+        <View style={{ width: "85%" }}>
+          <TextInput
+            onChangeText={formik.handleChange("email")}
+            autoFocus
+            dense
+            onBlur={formik.handleBlur("email")}
+            value={formik.values.email}
+            mode="outlined"
+            label="Email"
+            keyboardType="email-address"
+            placeholder="Enter Email"
+            style={{
+              backgroundColor: "white",
+            }}
+          />
+          <View style={{ height: 20 }}>
+            <Text
+              style={{
+                marginTop: 5,
+                marginLeft: 10,
+                color: theme.colors.error,
+              }}
+            >
+              {(formik.touched["email"] && formik?.errors?.email) || ""}
+            </Text>
+          </View>
+
+          <TextInput
+            onChangeText={formik.handleChange("password")}
+            dense
+            onBlur={formik.handleBlur("password")}
+            value={formik.values.password}
+            mode="outlined"
+            label="Password"
+            placeholder="Enter Password"
+            style={{
+              backgroundColor: "white",
+              marginTop: 10,
+            }}
+            // secureTextEntry={secretMap["password"]}
+            right={
+              <TextInput.Icon
+                style={{ paddingBottom: 4 }}
+                onPress={handlePassToggle("password")}
+                icon="eye"
+              />
+            }
+          />
+          <View style={{ height: 20 }}>
+            <Text
+              style={{
+                marginTop: 5,
+                marginLeft: 10,
+                color: theme.colors.error,
+              }}
+            >
+              {(formik.touched["password"] && formik?.errors?.password) || ""}
+            </Text>
+          </View>
+
+          <TextInput
+            onChangeText={formik.handleChange("confirmPassword")}
+            dense
+            onBlur={formik.handleBlur("confirmPassword")}
+            value={formik.values.confirmPassword}
+            mode="outlined"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            style={{
+              backgroundColor: "white",
+              marginTop: 10,
+            }}
+            // secureTextEntry={secretMap["confirmPassword"]}
+            right={
+              <TextInput.Icon
+                style={{ paddingBottom: 4 }}
+                onPress={handlePassToggle("confirmPassword")}
+                icon="eye"
+              />
+            }
+          />
+          <View style={{ height: 20 }}>
+            <Text
+              style={{
+                marginTop: 5,
+                marginLeft: 10,
+                color: theme.colors.error,
+              }}
+            >
+              {(formik.touched["confirmPassword"] &&
+                formik?.errors?.confirmPassword) ||
+                ""}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ alignItems: "center", marginTop: 20 }}>
+        <ProductoButton
+          loading={registerApiResult.isLoading}
+          type="contained"
+          // disabled={!email || !password || (!confirmPassword && !error)}
+          title="Register"
+          onPress={formik.handleSubmit}
+          color="primary"
+        />
+        <Text style={{ color: theme.colors.primary, marginTop: 20 }} h5>
+          Already have an account?
+        </Text>
+        <Text
+          h6
+          onPress={() => navigation.navigate("Login")}
+          style={{
+            color: "black",
+            marginTop: 5,
+            fontWeight: "700",
+          }}
+        >
+          Log-in Here
+        </Text>
+      </View>
+    </LayoutView>
   );
 };
 
@@ -162,8 +236,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
   },
   input: {
     width: 300,
