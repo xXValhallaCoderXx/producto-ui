a13593a9645175e1a29c3db5a761ba12ef2aa6c1
Renate <renate.gouveia@gmail.com>
2023-04-14 14:46:19 +0800

    Increase build number

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ module.exports = ({ config }) => {
     },
     android: {
       package: "com.bloopdevstudios.producto",
-      versionCode: 1,
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: "./assets/splash-icon.png",
         backgroundColor: "#FFFFFF",
@@ -16,16 +16,17 @@ module.exports = ({ config }) => {
     ios: {
       bundleIdentifier: "com.bloopdevstudios.producto",
       supportsTablet: true,
+      buildNumber: 2,
     },
     updates: {
       fallbackToCacheTimeout: 0,
       url: "https://u.expo.dev/91edd9b2-7766-4ed5-88e8-8821573af2f6",
     },
     extra: {
-      environment: "development",
-      baseUrl: "http://192.168.10.132:3000",
-      // environment: "production",
-      // baseUrl: "https://producto-dev.herokuapp.com",
+      // environment: "development",
+      // baseUrl: "http://192.168.10.132:3000",
+      environment: "production",
+      baseUrl: "https://producto-dev.herokuapp.com",
       eas: {
         projectId: "91edd9b2-7766-4ed5-88e8-8821573af2f6",
       },
